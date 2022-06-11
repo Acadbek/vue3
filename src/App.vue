85fbc64254960cede49c65f93e66f0f83fcc09b8
Asadbek <a.nosirjonov@uic.group>
2022-06-11 19:41:06 +0500

    feat: fetch function in composition api

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,31 @@
 <template>
   <div>
-    <input @keyup.enter="addUser" type="text" v-model="inputValue" placeholder="add user">
-    <button @click="array.length = 0">Click</button>
-    <div v-for="(user, index) in array" :key="index">
-      <p @click="deleteUser(index)"> {{ user }}</p>
-    </div>
+    <ul style="text-align: center">
+      <li v-for="(item, i) in users" :key="i">
+       <h1>
+        {{ item.name }}
+       </h1>
+       <h3> {{item.username}} </h3>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { ref, computed, watch } from 'vue'
+import { ref } from 'vue'
+import axios from 'axios'
+
 export default {
-  setup () {
-    let inputValue = ref('')
-    let array = ref(['asad', 'jasur'])
-    let sorted = ref([])
-
-    const addUser = () => {
-      array.value.push(inputValue.value)
-      console.log(array.value)
-      inputValue.value = ''
+  setup() {
+    let users = ref([])
+    
+    const getData = async () => {
+      let res = await axios.get('http://localhost:3000/users')
+      console.log(res.data)
+      users.value = await res.data
     }
-
-    const watchChanges = watch(array.value, () => {
-      console.log('watch')
-    })
-
-    const deleteUser = (index) => {
-      console.log('delete')
-      return array.value.splice(index, 1)
-    }
-
-    // const search = computed(() => {
-    //   return array.value.filter((element) => element.includes(inputValue.value))
-    // })
-
-    return { addUser, inputValue, array, watchChanges, watch, computed, deleteUser, sorted }
+    getData()
+    return { users }
   }
 }
 </script>
